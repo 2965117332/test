7da84e6b979843d98722e524d946d77222732813
2965117332 <2965117332@qq.com>
2018-05-31 11:53:47 +0800

    111

---
+++ web22/src/test/java/tt/NewTest.java
@@ -0,0 +1,22 @@
+package tt;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.out.println("hello");
+	   }
+  @Test
+  public void f1() {
+	  System.out.println("aa");
+	  }
+  @Test
+  public void f2() {
+	  System.out.println("bb");
+	  }
+  @Test
+  public void f3() {
+	  System.out.println("cc");
+	  }
+}
